a43e702054217dfa97d9ef2278d8c54e27c7f828
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-15 07:24:03 -0700

    yes

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is the function that runs printin the text specified
+ * Return: 0 is returned if function is succesful 
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
