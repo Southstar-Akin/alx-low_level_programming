3aef85ededdacdce053f074e92d064975ba22a5a
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-16 05:35:10 -0700

    yes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is a program that prints the belo line of text to standard error
+ * Return: returns 1 if successful
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
