4bfdc6defbbecf5cab970ba6e5471fccd0765bca
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-16 05:12:22 -0700

    si vous please,

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,6 @@ printf("Size of an int: %d byte(s)\n", sizeof(short int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", sizeof(float));
+fprintf(stderr, "Anything\n");
 return (0);
 }
