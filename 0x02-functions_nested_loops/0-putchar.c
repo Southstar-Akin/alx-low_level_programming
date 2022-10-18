5516b879fb070c440d46c17f8d9eb9efaee76eb0
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-18 14:55:20 -0700

    printing putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this calls the function line
+ * i hope this works on the first try
+ * Return: returns 0 on success
+ */
+
+int line()
+{
+printf("\_putchar\n");
+return (0);
+}
+int main()
+{
+line();
+return (0);
+}
