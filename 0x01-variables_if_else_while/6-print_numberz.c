fd99b1a4137bb87b0389558250fc8099010eeaea
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-17 17:53:52 -0700

    lets go

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - who am i if not seen through your eyes
+ * this is just a lyric from a song i jamming
+ * Return: 0 if successful
+ */
+int main(void)
+{
+int a = '1';
+for (a = 1; a < '10'; a++)
+{
+putcharf(a);
+}
+putchar('\n');
+return (0);
+}
