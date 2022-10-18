2f880e11c34984451c1988784e8a042917bf01b8
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-18 11:33:20 -0700

    yes

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,8 +6,16 @@
  */
 int main(void)
 {
-char C[] = '0123456789abcdef';
+char C;
+char d;
+for (C = 0; C < 10; C++)
+{
 putchar(C);
+}
+for (d = 'a'; d < 'f'; d++)
+{
+putchar(d);
+}
 putchar('\n');
 return (0);
 }
