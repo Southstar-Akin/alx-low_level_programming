a1c03e16addd6d4f803eda53a3c455bc45ad5b50
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-17 18:02:58 -0700

    please save ,e

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this na code
+ * no even whine me
+ * Return: returns 0 idf successful
+ */
+int main(void)
+{
+char C;
+for (C = 'z'; C >= 'a'; C--)
+{
+putchar(C);
+}
+putchar('\n');
+return (0);
+}
