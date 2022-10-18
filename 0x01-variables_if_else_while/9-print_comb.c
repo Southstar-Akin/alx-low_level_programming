84d423afe0b0d5575a0f1e3461801b989c118989
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-18 12:01:35 -0700

    yes

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,16 +10,13 @@ int main(void)
 int d = '0';
 for (d = '0'; d <= '9'; d++)
 {
+putchar(d);
 if (d != '9')
 {
-putchar(d);
 putchar(',');
 putchar(' ');
 }
-else
-{
-putchar(d);
 }
-}
+putchar('\n');
 return (0);
 }
