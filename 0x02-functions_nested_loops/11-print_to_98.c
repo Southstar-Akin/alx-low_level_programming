432bb4e20e0e9bf6a221868d68db86866ab292c8
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-19 16:52:48 -0700

    Fly with me, Cos i've been running too long

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,15 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * print_to_98 - print the numbers from n to 98
  *
- *
- *
+ * Return- always return 0
  */
 void print_to_98(int n)
 {
-while( n <= 98)
+while (n <= 98)
+{
 _putchar(n + '0');
 _putchar(',');
 _putchar(' ');
 n++;
+}
 }
