09e8d9117252753ab1b9d8978790d1d4046afcfe
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-19 16:22:08 -0700

    Fly with me, Cos i've been running to you

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - adds two interger
+ * 
+ * Return: returns sum of integers
+ */
+int add(int a, int b)
+{
+int result = a + b;
+printf("%d", result);
+return (result);
+}
