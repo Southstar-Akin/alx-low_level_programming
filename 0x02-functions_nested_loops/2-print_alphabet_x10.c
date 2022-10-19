4ba4413a38c7a687d650c97cc9607ce06f7decbf
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-19 04:31:57 -0700

    yass

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabets 10 times
+ */
+void print_alphabet_x10(void)
+{
+int t;
+for(t = 0; t <= 10; t++)
+{
+char A = 'a'
+while (A <= 'z')
+{
+_putchar(A);
+}
+_putchar('\n');
+}
+}
