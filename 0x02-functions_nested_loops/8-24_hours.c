9012fb8c865a4dfdc495236dc852771ee8a42951
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-19 14:43:21 -0700

    yeah

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,7 +18,16 @@ for (c = 0; c <= 5; c++)
 {
 for (d = 0; d <= 9; d++)
 {
-if (a < 3 && b < 4)
+if (a != 2)
+{
+_putchar(a + '0');
+_putchar(b + '0');
+_putchar(':');
+_putchar(c + '0');
+_putchar(d + '0');
+_putchar('\n');
+}
+else if (a < 3 && b < 4)
 {
 _putchar(a + '0');
 _putchar(b + '0');
