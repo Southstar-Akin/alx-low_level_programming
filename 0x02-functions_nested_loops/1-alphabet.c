8e3a89f76a4de77f4ba838696c691367833e208d
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-19 03:43:46 -0700

    yeah

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,6 @@
  * don't fo
  * Return: alwasy return 0
  */
-void print_alphabet(void)
-{
-char A;
-for (A = 'a'; A <= 'z'; A++)
-{
-_putchar(A)
-}
-}
 int main(void)
 {
 print_alphabet();
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,15 @@
+#include <stdio.h>
 void charm(void)
 {
 printf("_putchar");
 }
-void print_alphabet(void);
+void print_alphabet(void)
+{
+char A;
+for (A = 'a'; A <= 'z'; A++)
+{
+_putchar(A)
+}
+}
 
 
