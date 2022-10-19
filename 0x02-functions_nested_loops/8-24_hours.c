8c85673e6f91a3effdebc092102a8c721e6ed3f6
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-19 09:39:55 -0700

    Yesterday

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,11 +12,13 @@ int c = 0;
 int d = 0;
 while(a <= 2)
 {
-  while (b <= 3)
-    {
+while (b <= 3)
+{
 while (c <= 5)
 {
 while (d <= 9)
+{
+if(d != 9)
 {
 _putchar(a + '0');
 _putchar(b + '0');
@@ -25,6 +27,17 @@ _putchar(c + '0');
 _putchar(d + '0');
 _putchar('\n');
 d++;
+ }
+else
+{
+_putchar(a + '0');
+_putchar(b + '0');
+_putchar(':');
+_putchar(c + '0');
+_putchar(d + '0');
+_putchar('\n');
+d = 0;
+    }
 }
 c++;
 }
