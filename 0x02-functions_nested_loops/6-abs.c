6f5257b59714bee8a6b913ae01142b917b0b373f
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-19 06:08:20 -0700

    Sanity

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - this is a function that return the absolute value of integers
+ * @c: value of integers
+ *
+ * Return: absolute value of argument
+ */
+int _abs(int c)
+{
+int result = abs(c);
+return (result);
+}
+  
