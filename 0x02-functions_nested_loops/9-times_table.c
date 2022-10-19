1639e57517c57165355b19ac3525c95606ee5265
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-19 15:51:08 -0700

    Euphoria

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,35 +8,35 @@
 void times_table(void)
 {
 int x = 0;
-while (x <= '9')
+while (x <= 9)
 {
 printf("%d", x * 0);
 printf(",");
-printf(" ");
+printf("  ");
 printf("%d", x * 1);
 printf(",");
-printf(" ");
+printf("  ");
 printf("%d", x * 2);
 printf(",");
-printf(" ");
+printf("  ");
 printf("%d", x * 3);
 printf(",");
-printf(" ");
+printf("  ");
 printf("%d", x * 4);
 printf(",");
-printf(" ");
+printf("  ");
 printf("%d", x * 5);
 printf(",");
-printf(" ");
+printf("  ");
 printf("%d", x * 6);
 printf(",");
-printf(" ");
+printf("  ");
 printf("%d", x * 7);
 printf(",");
-printf(" ");
+printf("  ");
 printf("%d", x * 8);
 printf(",");
-printf(" ");
+printf("  ");
 printf("%2d", x * 9);
 printf("\n");
 x++;
