9d289dd8045c73ec21b89b16d8100480ffa5fe40
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-19 03:10:07 -0700

    coffee and food

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,20 @@
 #include "main.h"
 /**
  * main - run the code
- *
+ * don't fo
  * Return: alwasy return 0
  */
+void print_alphabet(void)
+{
+char A;
+for (A = 'a'; A <= 'z'; A++)
+{
+_putchar(A)
+}
+}
 int main(void)
 {
-print_alph();
-putchar('\n');
+print_alphabet();
+_putchar('\n');
 return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,4 @@
-#include <stdio.h>
 void *line(void);
-void *print_alph(void)
-{
-int A;
-for (A = 'a'; A <= 'z'; A++)
-{
-putchar(A);
-}
-return (0); 
-}
+void *print_alphabet(void);
 
 
