bb12486d6085b68f30b7c1e5374b3db76ba5ed4a
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-19 03:29:09 -0700

    yeah

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**                                                                                                                                                                                 
+ * main - this calls the function line                                                                                                                                              
+ * i hope this works on the first try                                                                                                                                               
+ * Return: returns 0 on success                                                                                                                                                     
+ */
+int main(void)
+{
+_putchar();
+putchar('\n');
+return (0);
+}
