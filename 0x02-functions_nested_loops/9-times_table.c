fb53477225fdec9df24844d026cdb5e307ec57e8
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-19 16:06:10 -0700

    Euphoria

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,10 +5,39 @@
  * void
  * Return: returns no value
  */
-void times_table(void)
+void before(void)
 {
-int x = 0;
-while (x <= 9)
+printf("%d", x * 0);
+printf(",");
+printf(" ");
+printf("%d", x * 1);
+printf(",");
+printf("  ");
+printf("%d", x * 2);
+printf(",");
+printf(" ");
+printf("%d", x * 3);
+printf(",");
+printf(" ");
+printf("%d", x * 4);
+printf(",");
+printf(" ");
+printf("%d", x * 5);
+printf(",");
+printf(" ");
+printf("%d", x * 6);
+printf(",");
+printf(" ");
+printf("%d", x * 7);
+printf(",");
+printf(" ");
+printf("%d", x * 8);
+printf(",");
+printf(" ");
+printf("%d", x * 9);
+printf("\n");
+}
+void normal(void)
 {
 printf("%2d", x * 0);
 printf(",");
@@ -39,6 +68,20 @@ printf(",");
 printf(" ");
 printf("%2d", x * 9);
 printf("\n");
+}
+void times_table(void)
+{
+int x = 0;
+while (x <= 9)
+{
+if (x < 3)
+{
+before()
+}
+else
+{
+normal()
+}
 x++;
 }
 }
