60b1e41e23521eccaebc14ded3d20a2b29c2d13f
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-19 09:32:06 -0700

    Yesterday

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,18 +6,30 @@
  */
 void jack_bauer(void)
 {
-int h = 00;
-int mi = 00;
-while(h <= 24)
+int a = 0;
+int b = 0;
+int c = 0;
+int d = 0;
+while(a <= 2)
 {
-for(mi = 00; mi <= 59; mi++)
+  while (b <= 3)
+    {
+while (c <= 5)
 {
-_putchar(h);
+while (d <= 9)
+{
+_putchar(a);
+_putchar(b);
 _putchar(':');
-_putchar(mi);
+_putchar(c);
+_putchar(d);
 _putchar('\n');
+d++
 }
-h++;
-_putchar('\n');
+c++;
+}
+b++;
+}
+a++;
 }
 }
