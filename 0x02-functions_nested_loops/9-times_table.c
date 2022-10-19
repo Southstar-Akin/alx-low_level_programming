d0f97b95ebf0ae33a47adbc8f60a483206a149f6
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-19 15:32:25 -0700

    Euphoria

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,42 +7,38 @@
  */
 void times_table(void)
 {
-int t;
 int x = 0;
-for (t = 0; t <= 9; t++)
-{
 while (x <= 9)
 {
-_putchar((t * 0) + '0');
+_putchar((x * 0) + '0');
 _putchar(',');
 _putchar(' ');
-_putchar((t * 1) + '0');
+_putchar((x * 1) + '0');
 _putchar(',');
 _putchar(' ');
-_putchar((t * 2) + '0');
+_putchar((x * 2) + '0');
 _putchar(',');
 _putchar(' ');
-_putchar((t * 3) + '0');
+_putchar((x * 3) + '0');
 _putchar(',');
 _putchar(' ');
-_putchar((t * 4) + '0');
+_putchar((x * 4) + '0');
 _putchar(',');
 _putchar(' ');
-_putchar((t * 5) + '0');
+_putchar((x * 5) + '0');
 _putchar(',');
 _putchar(' ');
-_putchar((t * 6) + '0');
+_putchar((x * 6) + '0');
 _putchar(',');
 _putchar(' ');
-_putchar((t * 7) + '0');
+_putchar((x * 7) + '0');
 _putchar(',');
 _putchar(' ');
-_putchar((t * 8) + '0');
+_putchar((x * 8) + '0');
 _putchar(',');
 _putchar(' ');
-_putchar((t * 9) + '0');
+_putchar((x * 9) + '0');
+_putchar('\n');
 x++;
-}
-printf("\n");
 }
 }
