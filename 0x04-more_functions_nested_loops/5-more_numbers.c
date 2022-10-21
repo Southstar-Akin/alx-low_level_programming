34276010f13496ebf38ae56bbdb74cdf17e67726
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-21 04:39:37 -0700

    ueah2

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - prints number from 0 to 14 10 times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+int a = 1;
+while (a <= 10)
+{
+int b;
+for (b = 0; b <= 14; b++)
+{
+printf("%d", b);
+}
+_putchar('\n');
+a++;
+}
+}
