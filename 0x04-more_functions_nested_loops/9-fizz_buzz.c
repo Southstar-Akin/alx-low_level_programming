01d4b3b99f1ba446375f99834f38ae3a5c9f970b
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-21 11:19:24 -0700

    yeah

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,11 +9,11 @@ int main(void)
 int a;
 for (a = 1; a <= 100; a++)
 {
-if (a % 3 == 0)
+if (a % 3 == 0 && a % 5 != 0)
 {
 printf(" Fizz");
 }
-else if (a % 5 == 0)
+else if (a % 5 == 0 && a % 3 != 0)
 {
 printf(" Buzz");
 }
