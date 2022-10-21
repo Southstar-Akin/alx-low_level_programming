fb2ec8ad4a3dea87504d5e56e01bc653ce156a9d
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-21 04:20:35 -0700

    fly with me

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies the two arguments given to program
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: returns the result of the multiplication
+ */
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+return (result);
+}
