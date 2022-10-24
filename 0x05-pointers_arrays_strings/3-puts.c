77ea8f3ab9aba7d4f743c5ccb57be37d9e485e6c
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-24 13:12:00 -0700

    Puts function in the arrays pointers and functions section

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - prints the string or character to standard ouput
+ * @str: is the string passed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+puts(str);
+}
