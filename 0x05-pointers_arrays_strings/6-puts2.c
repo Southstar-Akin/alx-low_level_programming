7890842555074287d7821174f0c8d1f7d4fb4bf5
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-24 14:56:43 -0700

    Function to print all the odd indexed characters in string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts2 - prints every other cahracter in string
+ * @str: string param
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+int s = strlen(str);
+int j = 0;
+while (j <= s)
+{
+if(s % 2 == 0)
+{
+continue;
+}
+else
+{
+if (j != s - 1)
+{
+printf("%c", s);
+}
+else
+{
+printf("%c\n", s);
+}
+}
+j++;
+}
+}
