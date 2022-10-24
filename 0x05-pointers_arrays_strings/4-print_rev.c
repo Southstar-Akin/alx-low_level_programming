1f2a40735b2f367fb23c8976c21fa90f603b0d04
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-24 13:18:24 -0700

    Function that prints the string passed in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - primts the string characters in reverse
+ * @s: paramter passed
+ *
+ * Return: returns void
+ */
+void print_rev(char *s)
+{
+printf("%s", strrev(s));
+}
