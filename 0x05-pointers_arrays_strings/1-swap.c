ce163bd651d875ac87fedaed706ce441fc23fe24
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-24 11:37:59 -0700

    C function to swap values of declared intergers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of the ints
+ * @a: first int
+ * @b: second int
+ *
+ * Return: Void
+ */
+void swap_int(int *a, int *b)
+{
+*a = b;
+*b = a;
+}
