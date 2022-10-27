121cbd4356da9031e33978f5b7b4149a22bd1bff
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-27 16:50:45 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strcat - concatenates strings
+ * @dest: destination string
+ * @src: string to be concatenated
+ *
+ * Return: concatenated string
+ */
+char _strcat(char *dest, char *src)
+{
+strcat(dest, src);
+dest[strlen(dest) - 1] = '\0';
+return (dest);
+}
