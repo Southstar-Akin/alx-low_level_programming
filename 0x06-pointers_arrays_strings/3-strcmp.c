0752c17e127627ac4f247a52cb39fcd5b853d0ab
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-27 17:47:41 -0700

    yeah

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - compares two string
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: returns an interger 0 <= x >= 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+int res = strcmp(s1, s2);
+return (res);
+}
