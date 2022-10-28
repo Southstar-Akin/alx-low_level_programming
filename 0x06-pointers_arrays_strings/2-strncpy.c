f92fd11acf8d6daa676c2110d40a0bb88b7055af
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-27 17:31:38 -0700

    function tha copies string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source to copy valuues
+ * @n: Number of characters to copy
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+strncpy(dest, src n);
+return (dest);
+}
