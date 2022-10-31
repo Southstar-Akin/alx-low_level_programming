7bd4fd409f0a452c0aa9dcf21a905d49112ab739
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-31 16:33:31 -0700

    Changing the memory space size

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_memset - changes the memory spca of s yo constant b
+ * @s: first param
+ * @b: second param
+ * @n: last param
+ *
+ * Return: the pointer adress
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+int j = 0;
+while(j < n;)
+{
+s[i] = b;
+n++;
+}
+return (s)
+}
