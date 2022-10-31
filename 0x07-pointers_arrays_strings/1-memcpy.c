469847837a2c166d0f6834f566ec3b3ed8095ad3
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-10-31 16:58:42 -0700

    pointer

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *memcpy - copies n values of src to dest
+ * @dest: pointer
+ * @src: pointer
+ * @n: var
+ *
+ * Return: pointer dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+memcpy(dest, src, n);
+return (dest);
+}
