70e27e1976497c34ddad89a362cb64111ac7733f
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-11-01 17:48:40 -0700

    Strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strstr - finds substring in theis function
+ * @haystack: param
+ * @needle: param
+ *
+ * Return- substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+strstr(haystack, needle);
+return (*haystack);
+}
