a566986edae95be941a0aef034ab36c5279e835d
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-11-02 09:37:20 -0700

    lemme

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,14 +1,21 @@
 #include "main.h"
 #include <string.h>
 /**
- * _strstr - finds substring in theis function
+ * *_strstr - searches for cahracter c in string
  * @haystack: param
  * @needle: param
  *
- * Return- substring
+ * Return: 0 or null(if unsucessful)
  */
 char *_strstr(char *haystack, char *needle)
 {
-strstr(haystack, needle);
+char *res = strstr(haystack, needle);
+if (res != NULL)
+{
 return (haystack);
+}
+else
+{
+return (res);
+}
 }
