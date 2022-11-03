08444942bbadc943cd367b0e3d3336cfe3e0e971
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-11-03 12:06:02 -0700

    yes

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - prints a string to standard output
+ * @s: param
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+puts(s);
+}
