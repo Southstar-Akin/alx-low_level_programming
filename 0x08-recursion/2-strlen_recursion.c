841ab045520d29dd4e88e3b382e88b510aa66153
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-11-03 13:03:27 -0700

    string lenght printing

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen_recursion - prints string
+ * @s: param
+ *
+ * Return: lenght odf string
+ */
+int _strlen_recursion(char *s)
+{
+int res = strlen(s);
+return (res);
+}
