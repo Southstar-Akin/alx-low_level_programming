15d50887c67a3b529e7e28bc36b179ff35891108
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-11-03 15:58:37 -0700

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * is_prime_number - prints prome number
+ * @n: param
+ *
+ * Return: void
+ */
+int is_prime_number(int n)
+{
+if (n % 1 == 0 && n % n == 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
