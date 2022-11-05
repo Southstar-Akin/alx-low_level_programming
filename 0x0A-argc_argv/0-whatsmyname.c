d9c8842edc53b0fd1ae7ba67273b337900494631
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-11-04 20:54:12 -0700

    number of arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,15 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 /**
  * main - prints the file name
  * @argc: first param
  * @argv: second param
  *
+ * Return: 0 if successful
  */
 int main(int argc, char* argv[])
 {
 (void)argc;
-printf("%s", argv[0]);
-printf("\n");
+printf("%s\n", argv[0]);
 return (0);
 }
