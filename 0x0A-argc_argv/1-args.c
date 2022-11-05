8554d458eaabd017c1ead24a1079ae23e2971181
oluwaseyi <akinbileoluwaseyi2001@gmail.com>
2022-11-04 20:56:52 -0700

    number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,11 +5,11 @@
  * @argc: first param
  * @argv: second param
  *
+ * Return: 0 if successful
  */
 int main(int argc, char* argv[])
 {
 (void)argv;
-printf("%d", argc);
-printf("\n");
+printf("%d\n", argc - 1);
 return (0);
 }
